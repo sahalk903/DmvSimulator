aed0e6733c16c5e63b54e4870b0f0356783ebeb0
sahalk903 <sydneyhalk@gmail.com>
2021-03-01 13:07:19 -0500

    first parts of code

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,6 +1,20 @@
+import java.util.Random;
 
 public class DmvSimulator {
 	public static void main(String[] args) {
-		
-	}
+	
+			
+			Random rand = new Random();
+			
+			
+			int rand_int1 = rand.nextInt(100);
+			
+			System.out.println("Welcome to the DMV!");
+			
+			System.out.println("This is your number wait to be called on:"+rand_int1);
+			
+			}
+
 }
+
+
